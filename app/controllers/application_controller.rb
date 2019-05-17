f6876a899d4e67260b6a35532a58e3acdc5de5ed
Andrea <anmastrantoni@gmail.com>
2019-05-17 12:52:21 +0000

    Finished Adding Basic Admin Authication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,5 +38,11 @@ class ApplicationController < ActionController::Base
     redirect_to '/login' unless current_user
   end
 
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end
+
 
 end
