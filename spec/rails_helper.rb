09e19c71d0d4ac44dae01c22692d5df721ec0d32
Andrea <anmastrantoni@gmail.com>
2019-05-21 17:47:39 +0000

    added freature test for home page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require "capybara/rails"
-require "capybara/rspec"
-require "capybara/poltergeist"
+
+
+
 
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -9,6 +9,10 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rspec'
+require "capybara/poltergeist"
+Capybara.javascript_driver = :poltergeist
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -65,6 +69,5 @@ RSpec.configure do |config|
 
   # Specs flagged with `js: true` will use Capybara's JS driver. Set
   # that JS driver to :poltergeist
-  Capybara.javascript_driver = :poltergeist
 
 end
